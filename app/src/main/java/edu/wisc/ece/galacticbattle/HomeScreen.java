259aa21205e7ea1968337ee5e8b49da09327186f
Blake <bdjansen17@gmail.com>
2016-10-17 17:42:08 -0500

    Added in more xml, started options

--- app/src/main/java/edu/wisc/ece/galacticbattle/HomeScreen.java
+++ app/src/main/java/edu/wisc/ece/galacticbattle/HomeScreen.java
@@ -1,7 +1,9 @@
 package edu.wisc.ece.galacticbattle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,8 +11,19 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
 
-        // Comment af
-        // testing
+    public void searchPlayers(View view) {
+        // Go back to the main activity
+        Intent mIntent = new Intent(HomeScreen.this,
+                GameActivity.class);
+        startActivity(mIntent);
+    }
+
+    public void openOptions(View view) {
+        // Go back to the main activity
+        Intent mIntent = new Intent(HomeScreen.this,
+                OptionsActivity.class);
+        startActivity(mIntent);
     }
 }
---
+++ app/src/main/java/edu/wisc/ece/galacticbattle/OptionsActivity.java
@@ -0,0 +1,52 @@
+package edu.wisc.ece.galacticbattle;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Blake on 10/17/2016.
+ */
+
+public class OptionsActivity extends AppCompatActivity {
+    private boolean changedSpeed = false;
+    private boolean changedColors = false;
+    private boolean clearedStats = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+
+    private void loadUserData() {
+        // Create the shared preferences variable so we can load in the data
+        String mKey = getString(R.string.preference_name);
+        SharedPreferences mPrefs = getSharedPreferences(mKey, MODE_PRIVATE);
+
+        // Load the string of all the names and then split them by the correct character
+        mKey = getString(R.string.preference_key_profile_colors);
+        //TODO: Put in loading colors
+
+        mKey = getString(R.string.preference_key_profile_speed);
+        //TODO: Put in loading speed
+    }
+
+    private void saveUserData() {
+        String mKey = getString(R.string.preference_name);
+        SharedPreferences mPrefs = getSharedPreferences(mKey, MODE_PRIVATE);
+
+        //Create our editor and clear it to put the new data into preferences
+        SharedPreferences.Editor mEditor = mPrefs.edit();
+        mEditor.clear();
+
+        mKey = getString(R.string.preference_key_profile_colors);
+        //TODO: Put in saving colors
+
+        mKey = getString(R.string.preference_key_profile_speed);
+        //TODO: Put in saving speed
+
+        // Officially commit the changes to the shared preferences
+        mEditor.commit();
+    }
+}

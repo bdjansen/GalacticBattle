4b9602528df0a07459f8f08d893eb35794af5af7
Blake <bdjansen17@gmail.com>
2016-10-24 17:03:57 -0500

    Added logic to the ship and bullet, changed some workflow between the screens

--- app/src/main/java/edu/wisc/ece/galacticbattle/Spaceship.java
+++ app/src/main/java/edu/wisc/ece/galacticbattle/Spaceship.java
@@ -1,11 +1,16 @@
 package edu.wisc.ece.galacticbattle;
 
+import java.util.ArrayList;
+
 /**
  * Created by Blake on 10/17/2016.
  */
 public class Spaceship {
     private int x;
     private int y;
+    private int widthRadius;
+    private int heightRadius;
+    private int lives = 3;
 
     public Spaceship(int x, int y) {
         this.x = x;
@@ -36,5 +41,27 @@ public class Spaceship {
 
     public int getY() {
         return y;
+    }
+
+    public int getWidthRadius() { return widthRadius; }
+
+    public int getHeightRadius() { return heightRadius; }
+
+    public boolean isAlive() {
+        return lives > 0;
+    }
+
+    public boolean collisionDetection(ArrayList<Bullet> bullets) {
+        for (int i = 0; i < bullets.size(); i++) {
+            Bullet b = bullets.get(i);
+            if(b.getX() > x - widthRadius - b.getWidthRadius()
+                    && b.getX() < x + widthRadius + b.getWidthRadius()
+                    && b.getY() > y - heightRadius - b.getHeightRadius()
+                    && b.getY() < y + heightRadius + b.getHeightRadius()){
+                bullets.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
 }

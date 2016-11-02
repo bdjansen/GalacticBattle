5cb7f5363e0fadd71a29b6f91a07b0ec9fa4a6df
mfilliat <mfilliat@wisc.edu>
2016-11-02 16:57:58 -0500

    Matt's stuff 11/2/16

--- app/src/main/java/edu/wisc/ece/galacticbattle/FindPlayersActivity.java
+++ app/src/main/java/edu/wisc/ece/galacticbattle/FindPlayersActivity.java
@@ -1,5 +1,7 @@
 package edu.wisc.ece.galacticbattle;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
@@ -10,6 +12,7 @@ import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Blake on 10/24/2016.
@@ -18,6 +21,7 @@ public class FindPlayersActivity extends ListActivity {
     private ArrayAdapter<String> mAdapter;
     private ArrayList<String> FOLKS = new ArrayList<String>();
 
+    private int REQUEST_ENABLE_BT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,30 @@ public class FindPlayersActivity extends ListActivity {
 
         // Assign the adapter to ListView
         setListAdapter(mAdapter);
+
+        // Get the bluetooth adapter
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+        // Make sure bluetooth is enabled on this device
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                mAdapter.add(device.getName());
+            }
+        }
+
+
     }
 
     public void Go(View v) {
